16c8da12f1c53ad874e2b0675a6c77cb627bc0b7
Soup Bowl <cl-overflow@outlook.com>
2018-08-14 14:46:17 +0000

    Update provision.sh

--- mssql/provision.sh
+++ mssql/provision.sh
@@ -3,14 +3,15 @@
 echo "Installing UNIX ODBC and MSODBC."
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee --append /etc/apt/sources.list.d/mssql-release.list
-sudo ACCEPT_EULA=Y apt install unixodbc-dev msodbcsql17 -y
+sudo apt-get update > /dev/null
+sudo apt-get install unixodbc unixodbc-dev -y > /dev/null
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17 -y > /dev/null
 
 # Instructions from https://bit.ly/2w1fkCo
 # check (TODO):  php -m | grep -w 'pdo_sqlsrv' <OR> 'pdo_sqlsrsv'
 
 echo "Installing PHP Microsoft SQL extensions."
-sudo pecl install sqlsrv
-sudo pecl install pdo_sqlsrv
+sudo pecl install sqlsrv pdo_sqlsrv
 
 sudo bash -c "echo extension=sqlsrv.so > /etc/php/7.2/fpm/conf.d/sqlsrv.ini"
 sudo bash -c "echo extension=pdo_sqlsrv.so > /etc/php/7.2/fpm/conf.d/pdo_sqlsrv.ini"
