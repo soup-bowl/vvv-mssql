fb7a36108b85d477b80160dd344b87fbd8982229
Soup Bowl <cl-overflow@outlook.com>
2018-08-14 12:52:56 +0000

    Update provision.sh

--- mssql/provision.sh
+++ mssql/provision.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-# Todo
+echo "Installing MSSQL Connector for PHP."
+
+# Instructions from https://bit.ly/2w1fkCo
+sudo apt install unixodbc-dev -y
+sudo pecl install sqlsrv
+sudo pecl install pdo_sqlsrv
+
+echo "Finished. Please add the following to your php.ini file:"
+echo "extension=sqlsrv.so"
+echo "extension=pdo_sqlsrv.so"
