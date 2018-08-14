2b9a92107afceb5aef9f83e923533a3ac953af9b
Soup Bowl <cl-overflow@outlook.com>
2018-08-14 13:44:30 +0000

    Update mssql/provision.sh

--- mssql/provision.sh
+++ mssql/provision.sh
@@ -2,6 +2,11 @@
 
 echo "Installing MSSQL Connector for PHP."
 
+sudo su 
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+sudo ACCEPT_EULA=Y apt install msodbcsql17 -y
+
 # Instructions from https://bit.ly/2w1fkCo
 # check (TODO):  php -m | grep -w 'pdo_sqlsrv' <OR> 'pdo_sqlsrsv'
 
