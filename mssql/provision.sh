f4d67a7dc4e3995fcfa2c28d98e9bfa1e7970adc
Soup Bowl <cl-overflow@outlook.com>
2018-08-15 08:54:04 +0000

    After a grueling fight, bringing unixodbc in manually.

--- mssql/provision.sh
+++ mssql/provision.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
+# Instructions from https://bit.ly/2HlHjRW and https://bit.ly/2w1fkCo
+
 echo "Installing UNIX ODBC and MSODBC."
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee --append /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update > /dev/null
-sudo apt-get install unixodbc unixodbc-dev -y > /dev/null
+sudo apt-get install unixodbc -y > /dev/null
+# unixodbc-dev for 14.04 needs to be manually installed.
+curl http://launchpadlibrarian.net/174316120/unixodbc-dev_2.3.1-3_amd64.deb > ~/unixodbc-dev_2.3.1-3_amd64.deb
+sudo dpkg -i ~/unixodbc-dev_2.3.1-3_amd64.deb > /dev/null
+rm ~/unixodbc-dev_2.3.1-3_amd64.deb
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17 -y > /dev/null
 
-# Instructions from https://bit.ly/2w1fkCo
-# check (TODO):  php -m | grep -w 'pdo_sqlsrv' <OR> 'pdo_sqlsrsv'
-
 echo "Installing PHP Microsoft SQL extensions."
-sudo pecl channel-update pecl.php.net
-sudo pecl install sqlsrv pdo_sqlsrv
+sudo pecl channel-update pecl.php.net > /dev/null
+sudo pecl install sqlsrv pdo_sqlsrv > /dev/null
 
 sudo bash -c "echo extension=sqlsrv.so > /etc/php/7.2/fpm/conf.d/sqlsrv.ini"
 sudo bash -c "echo extension=pdo_sqlsrv.so > /etc/php/7.2/fpm/conf.d/pdo_sqlsrv.ini"
 
+echo "-----"
 echo "Finished. Please add the following to your php.ini file:"
 echo "extension=sqlsrv.so"
 echo "extension=pdo_sqlsrv.so"
+echo "-----"
